5f929dc7e60f27a91b57efcd0a6ddda397507b06
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-10-18 13:57:13 +0530

    Recycle view using Fragments

---
+++ app/src/main/java/com/example/demo/HomeFragment.kt
@@ -0,0 +1,116 @@
+package com.example.demo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var employeeList : ArrayList<Employee>
+    private lateinit var employeeadapter : EmployeeAdapter
+
+    lateinit var imageId:Array<Int>
+    lateinit var textId1:Array<String>
+    lateinit var textId2:Array<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        dataSetter()
+//
+//        recyclerView=view.findViewById(R.id.recyclerView)
+//        recyclerView.layoutManager= LinearLayoutManager(context)
+//        recyclerView.setHasFixedSize(true)
+//
+//        employeeadapter=EmployeeAdapter(employeeList)
+//        recyclerView.adapter=employeeadapter
+//    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dataSetter()
+
+        recyclerView=view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager= LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
+
+        employeeadapter=EmployeeAdapter(employeeList)
+        recyclerView.adapter=employeeadapter
+    }
+
+
+
+    private fun dataSetter() {
+
+        employeeList=ArrayList()
+
+        employeeList.add(Employee(R.drawable.naruto,"Naruto","Uzumaki"))
+        employeeList.add(Employee(R.drawable.bakuyu_hinata,"Hinata","Naruto"))
+        employeeList.add(Employee(R.drawable.uchiha_sasuke,"Sasuke","Uchiha"))
+        employeeList.add(Employee(R.drawable.uchiha_madara,"Madara","Uchiha"))
+        employeeList.add(Employee(R.drawable.uchiha_itachi,"Itachi","Uchiha"))
+        employeeList.add(Employee(R.drawable.hatake_kakashi,"Kakashi","Sensei"))
+        employeeList.add(Employee(R.drawable.sensei_jiraya,"Jiraya","Sensei"))
+        employeeList.add(Employee(R.drawable.lady_tsunade,"Tsunade","Lady"))
+        employeeList.add(Employee(R.drawable.minato,"Minato","Uzumaki"))
+        employeeList.add(Employee(R.drawable.uzumaki_kushina,"kushina","Uzumaki"))
+
+
+    }
+}
+
--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,40 +1,45 @@
-  package com.example.demo
+package com.example.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-  class MainActivity : AppCompatActivity() {
-      private lateinit var recyclerView : RecyclerView
-      private lateinit var employeeList : ArrayList<Employee>
-      private lateinit var employeeadapter : EmployeeAdapter
+class MainActivity : AppCompatActivity() {
 
-      override fun onCreate(savedInstanceState: Bundle?) {
-          super.onCreate(savedInstanceState)
-          setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-          recyclerView=findViewById(R.id.recyclerView)
-          recyclerView.setHasFixedSize(true)
-          recyclerView.layoutManager=LinearLayoutManager(this)
+        /*
+        recyclerView=findViewById(R.id.recyclerView)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager=LinearLayoutManager(this)
 
-          employeeList= ArrayList()
+        employeeList= ArrayList()
 
-          employeeList.add(Employee(R.drawable.naruto,"Naruto","Uzumaki"))
-          employeeList.add(Employee(R.drawable.bakuyu_hinata,"Hinata","Naruto"))
-          employeeList.add(Employee(R.drawable.uchiha_sasuke,"Sasuke","Uchiha"))
-          employeeList.add(Employee(R.drawable.uchiha_madara,"Madara","Uchiha"))
-          employeeList.add(Employee(R.drawable.uchiha_itachi,"Itachi","Uchiha"))
-          employeeList.add(Employee(R.drawable.hatake_kakashi,"Kakashi","Sensei"))
-          employeeList.add(Employee(R.drawable.sensei_jiraya,"Jiraya","Sensei"))
-          employeeList.add(Employee(R.drawable.lady_tsunade,"Tsunade","Lady"))
-          employeeList.add(Employee(R.drawable.minato,"Minato","Uzumaki"))
-          employeeList.add(Employee(R.drawable.uzumaki_kushina,"kushina","Uzumaki"))
+        employeeList.add(Employee(R.drawable.naruto,"Naruto","Uzumaki"))
+        employeeList.add(Employee(R.drawable.hinata,"Hinata","Naruto"))
+        employeeList.add(Employee(R.drawable.sasuke,"Sasuke","Uchiha"))
+        employeeList.add(Employee(R.drawable.madara,"Madara","Uchiha"))
+        employeeList.add(Employee(R.drawable.itachi,"Itachi","Uchiha"))
+        employeeList.add(Employee(R.drawable.kakshi,"Kakashi","Sensei"))
+        employeeList.add(Employee(R.drawable.jiraya,"Jiraya","Sensei"))
+        employeeList.add(Employee(R.drawable.tsunade,"Tsunade","Lady"))
+        employeeList.add(Employee(R.drawable.minato,"Minato","Uzumaki"))
+        employeeList.add(Employee(R.drawable.kushina,"kushina","Uzumaki"))
 
-          employeeadapter=EmployeeAdapter(employeeList)
-          recyclerView.adapter=employeeadapter
+        employeeadapter=EmployeeAdapter(employeeList)
+        recyclerView.adapter=employeeadapter
+        */
 
-      }
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(homeFragment: Fragment) {
+
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,homeFragment)
+        fragmentTransaction.commit()
+    }
 }
